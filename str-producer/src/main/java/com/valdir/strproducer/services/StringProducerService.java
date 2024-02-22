db764139a84a08f24aba9996f8c1f8b7212eabda
axcel0773 <yuji-watanabe1@hotmail.com>
2024-02-22 09:18:45 -0300

    Função callback

--- str-producer/src/main/java/com/valdir/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/valdir/strproducer/services/StringProducerService.java
@@ -1,9 +1,11 @@
 package com.valdir.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -12,6 +14,16 @@ public class StringProducerService {
 
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if (success != null) {
+                        log.info("Send message with success {}", message);
+                        log.info("Partition {}, Offset {}",
+                                success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send Message")
+        );
     }
 }
