205cb59d135ed8631a853c551994d09f06a0f75b
axcel0773 <yuji-watanabe1@hotmail.com>
2024-02-22 12:13:28 -0300

    Iniciando String Consumer

--- str-producer/src/main/java/com/valdir/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/valdir/strproducer/services/StringProducerService.java
@@ -14,16 +14,18 @@ public class StringProducerService {
 
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message).addCallback(
-                success -> {
-                    if (success != null) {
-                        log.info("Send message with success {}", message);
-                        log.info("Partition {}, Offset {}",
-                                success.getRecordMetadata().partition(),
-                                success.getRecordMetadata().offset());
-                    }
-                },
-                error -> log.error("Error send Message")
-        );
+        log.info("Send message {}", message);
+        kafkaTemplate.send("str-topic", message);
+//                .addCallback(
+//                success -> {
+//                    if (success != null) {
+//                        log.info("Send message with success {}", message);
+//                        log.info("Partition {}, Offset {}",
+//                                success.getRecordMetadata().partition(),
+//                                success.getRecordMetadata().offset());
+//                    }
+//                },
+//                error -> log.error("Error send Message")
+//        );
     }
 }
